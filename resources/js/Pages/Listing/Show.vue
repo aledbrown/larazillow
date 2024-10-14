d30ef1027e58e4b40b2ef8429bc52bb10453fd35
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-14 16:44:47 +0100

    Listing Show and Address Component
    Pretty good, simple enough

---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,13 @@
+<template>
+    <Link :href="`/listing`">Back</Link>
+    <div>Listing</div>
+    <ListingAddress :listing="listing" />
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import ListingAddress from "@/Components/ListingAddress.vue";
+defineProps({
+    listing: Object
+})
+</script>
