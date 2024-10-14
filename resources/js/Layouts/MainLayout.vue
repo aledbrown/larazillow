a516c84640c8702f3c223f167860a99cc4e520ed
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-14 10:24:01 +0100

    Persistent MainLayout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,16 @@
-<script setup>
-import { Link } from '@inertiajs/vue3';
-</script>
-
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
+
+    <div>The page with time is: {{ timer }}</div>
+
     <slot></slot>
 </template>
 
-<style scoped>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
-</style>
+const timer = ref(0)
+setInterval(() => timer.value++, 1000)
+</script>
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,14 +1,19 @@
 <template>
-    <MainLayout>
-        <div>Index using MainLayout this is working</div>
-        <div>{{ message }}</div>
-    </MainLayout>
+    <div>Index using MainLayout this is working</div>
+    <div>{{ message }}</div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
-import MainLayout from "../../Layouts/MainLayout.vue";
 defineProps({
     message: String
 })
+</script>
+
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+
+export default {
+    layout: MainLayout
+}
 </script>
