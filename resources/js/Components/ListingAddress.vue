9f85a9c42c52396569064f857b2b804fe5570eb2
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-16 09:31:04 +0100

    New Listing Space and Address changes

--- resources/js/Components/ListingAddress.vue
+++ resources/js/Components/ListingAddress.vue
@@ -1,6 +1,6 @@
 <template>
     <span>
-        {{ listing.street }} {{ listing.street_nr }}, {{ listing.city}}, &pound;{{ listing.price }}
+        {{ listing.street }} {{ listing.street_nr }}, {{ listing.city}}
     </span>
 </template>
 
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,7 +3,8 @@
         <Box v-for="listing in listings" :key="listing.id">
             <div>
                 <Link :href="route('listing.show', listing.id)">
-                    <ListingAddress :listing="listing" />
+                    <ListingSpace :listing="listing" class="text-lg" />
+                    <ListingAddress :listing="listing" class="text-gray-500" />
                 </Link>
             </div>
             <div class="text-2xl">
@@ -20,6 +21,7 @@
 import { Link } from "@inertiajs/vue3";
 import ListingAddress from "@/Components/ListingAddress.vue";
 import Box from "@/Components/UI/Box.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
 
 defineProps({
     listings: Array,
