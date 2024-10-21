dabf4617082f4970d07e5489ec6b9f84831772ee
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-10-21 15:09:35 +0100

    Styling continued

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,12 +1,33 @@
 <template>
-    <div><Link :href="route('listing.index')">Back</Link></div>
-    <div>Listing</div>
-    <ListingAddress :listing="listing" />
+    <div class="grid grid-cols-12 gap-4">
+        <Box class="col-span-12 md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">No images</div>
+        </Box>
+        <div class="order-first md:order-none col-span-12 md:col-span-5">
+            <Box>
+                <template #header>
+                    Basic info
+                </template>
+                <Price :price="listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />
+            </Box>
+            <Box class="mt-4">
+                <template #header>
+                    Offer
+                </template>
+                Making an offer
+            </Box>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import ListingAddress from "@/Components/ListingAddress.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import Price from "@/Components/Price.vue";
+import Box from "@/Components/UI/Box.vue";
 defineProps({
     listing: Object
 })
