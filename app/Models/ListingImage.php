75a1d6925b427d98fb2462cf75c15b30201ae019
Aled Brown <690500+aledbrown@users.noreply.github.com>
2024-11-07 08:58:04 +0000

    Listing Image model

---
+++ app/Models/ListingImage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ListingImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'filename',
+    ];
+
+    /**
+     * Get the user that owns the ListingImage
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Listing::class, 'listing_id');
+    }
+}
---
+++ database/migrations/2024_11_06_164241_create_listing_images_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('listing_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->foreignIdFor(model: Listing::class, column: 'listing_id')->constrained('listings');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('listing_images');
+    }
+};
